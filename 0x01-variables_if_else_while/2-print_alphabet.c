4e6b94af2a3c88d2bece8d6bf349d217bd4b6030
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-03 21:58:44 +0300

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,15 @@
 
 
 /**
- * main - func 
+ * main - the code begin
+ *
+ *
  *
  * It always return 0 if the code is correct
  */
 int main(void)
 {
+	/* your code goes there */
 	int alpha;
 
 	for (alpha = 97; alpha <= 122; alpha++)
