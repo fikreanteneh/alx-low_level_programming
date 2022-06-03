3f3f098e68695455298a77af6ecfaf661c4b4b6e
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-03 23:31:10 +0300

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,4 +8,24 @@
  * It always return 0 if the code is correct
  */
 
+int main(void)
+{
+	int a, b;
 
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (p == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
