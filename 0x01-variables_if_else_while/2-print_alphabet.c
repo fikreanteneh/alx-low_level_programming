9473fd46e66c20dfd45abe6b5db5352192884a1f
fikreanteneh <fikre.anteneh01@gmail.com>
2022-06-03 06:23:16 -0700

    1-3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - The code begins
+ *
+ * Return: always 0 if the code is correct
+ */
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 97; alpha <= 122; c++)
+	{
+		putchar(alpha);
+	}
+	printf("\n");
+	return (0);
+}
