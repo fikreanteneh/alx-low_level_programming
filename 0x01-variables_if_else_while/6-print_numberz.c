76b1578919e15f3f1e03d194366983608e454c25
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-04 09:43:57 +0300

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,12 @@
 /**
  * main - The code begins
  *
- * It always return 0 if the code is correct
+ * Return: It always return 0 if the code is correct
  */
 
 int main(void)
 {
+	/* your code goes there */
 	int digits;
 	
 	for (digits = '0'; digits <= '9'; digits++)
