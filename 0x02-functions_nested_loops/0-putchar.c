e9c4ac982c9a01a1cbc54cb218f898fa63236850
fikreanteneh <fikre.anteneh01@gmail.com>
2022-06-07 06:53:26 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - The code begins
+ * 
+ * Return: Always return 0 if the code is correct
+ */
+
+
+int main(void)
+{
+	char *abc="_putchar";
+
+	while (*abc != '\0')
+	{
+		putchar(*abc);
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+}
