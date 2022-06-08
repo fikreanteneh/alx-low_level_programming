887aaf393f04e754f2b59d96a39f651757de00db
fikreanteneh <fikre.anteneh01@gmail.com>
2022-06-07 23:05:31 -0700

    neccaary files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - The code begins
- * 
- * Return: returns noting
+ * print_alphabet - print all lowercase letters
+ *
+ * Return: null.
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	int alpha;
+	int stop = 122, count = 97;
 
-	for (alpha = 97; alpha <= 122; alpha++)
+	while (count <= stop)
 	{
-		putchar(alpha);
+		putchar(count);
+		count++;
 	}
-	putchar("\n");
-
+	putchar('\n');
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -17,23 +16,3 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
-=======
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
->>>>>>> 255d57c60dc15d4f356d7ba5d4e0655fecfb44a6
