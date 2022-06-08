eca2252cdab0a22805f10e87ed341671f16708d2
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-08 19:36:04 +0300

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,20 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
+if (n < 0)
 	{
+		if (n == INT_MIN)
+		{
+			long m = INT_MAX;
+
+			m++;
+			_putchar(m % 10 + '0');
+			return (m % 10);
+		}
 		n = -n;
+		_putchar(n % 10 + '0');
 		return (n % 10);
 	}
+	_putchar(n % 10 + '0');
 	return (n % 10);
 }
