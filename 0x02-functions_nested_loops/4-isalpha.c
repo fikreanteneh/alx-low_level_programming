9070fe6c339709ed6507c82a42175660d303b1a4
fikreanteneh <fikre.anteneh01@gmail.com>
2022-06-07 23:30:13 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a char is letter or not
+ *
+ * Return: 1 if c is letter, otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
+
