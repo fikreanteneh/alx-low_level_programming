7af98bf45329ef86c9d97a7682d1f58653001ee1
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 21:18:43 +0300

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print a adiagonal line in new line
+ * @n: number of diagonal
+ */
+
+void print_diagonal(int n)
+{
+  int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
