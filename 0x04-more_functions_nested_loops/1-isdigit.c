6e223ca5b819b88a7aede5c4f6aaf8ee7e0d2e4e
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 16:58:30 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,9 @@
  * @c: character
  * Return: return 1 if a digit else return 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
         if (c >= '0' && c <= '9')
-        {
 		return (1);
-	}
 	return (0);
-
+}
