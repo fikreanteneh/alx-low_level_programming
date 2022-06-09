f2ab81691bb720e19b2e3a743445447b2936e449
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 21:29:04 +0300

    Update 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - print square
+ * @size: integer number
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; ++a)
+		{
+			for (b = 0; b < size; ++b)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
