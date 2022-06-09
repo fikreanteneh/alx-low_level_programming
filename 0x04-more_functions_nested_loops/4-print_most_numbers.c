4ca54b2482ae1126bc2d719013f13143d3931920
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 17:17:10 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- *print_most_numbers - print number 0-9
+ * print_most_numbers - print numbers except 2 and 4
  */
 void print_most_numbers(void)
 {
 	int i = 48;
+
 	while (i < 60)
 	{
-    		if (i != 50 && i != 52)
-    		{
-			_putchar(i + '0');
+		if (i == 50 || i == 52)
+		{
+			i++;
+			continue;
 		}
+		_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');
