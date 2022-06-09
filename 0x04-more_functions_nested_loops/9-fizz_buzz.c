b1f374d276e9e6949f7e8c9579f42288b8570872
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 21:45:12 +0300

    Update 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Fizz and Buzz
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 101; ++a)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+		{
+			printf("FizzBuzz");
+      printf(" ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+      printf(" ");
+		}
+		else if (a % 5 == 0 && a != 100)
+		{
+      printf("Buzz");
+      printf(" ");
+    }
+		else if (a == 100)
+		{
+		  printf("Buzz");
+		 }
+     else
+     {
+		 printf("%ia ", i);
+		 }
+		}
+
+	}
+	printf("\n");
+	return (0);
+}
