bed41174faddf9cc80afa71d2494830147b946e0
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-09 17:07:22 +0300

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print number 0-9
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 60)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
