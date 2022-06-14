5ec5103f4b58b8c2e4c94197e4723e63be374395
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-06-14 15:57:47 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,19 @@
  */
 void print_rev(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	int i, len, temp;
 
 	len = 0;
-	len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	len1 = len - 1;
-
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - 1 -i] = temp;
 	}
 }
